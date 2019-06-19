41ffb7c4504c9243be5595e1e5e8ec3462d788a5
Anna <kostechuk.anna@gmail.com>
2019-06-19 10:58:30 +0800

    add gh-pages

---
+++ build/precache-manifest.6cb3ff2658d6d132b25400e53c819cfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fa0429bc1d23df8d800d6bda440fd8",
+    "url": "/cat-blog/index.html"
+  },
+  {
+    "revision": "a734005bc2eda2a46e76",
+    "url": "/cat-blog/static/css/main.839fe2b0.chunk.css"
+  },
+  {
+    "revision": "2ea84f698c4f08d9ce47",
+    "url": "/cat-blog/static/js/2.06cf24ea.chunk.js"
+  },
+  {
+    "revision": "a734005bc2eda2a46e76",
+    "url": "/cat-blog/static/js/main.7f895cb1.chunk.js"
+  },
+  {
+    "revision": "d726143a8f6afca7c9e4",
+    "url": "/cat-blog/static/js/runtime~main.c62db41a.js"
+  }
+]);
